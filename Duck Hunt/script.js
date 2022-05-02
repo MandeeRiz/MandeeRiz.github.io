ac97bb355b248bb11727fef093690bbc7aa87489
Mandee <102609655+MandeeRiz@users.noreply.github.com>
2022-05-02 19:49:29 -0400

    Add files via upload

---
+++ Duck Hunt/script.js
@@ -0,0 +1,17 @@
+// User object (points:, Ammo,)
+
+//computer object(points,)
+
+// onclick screen (-player1 ammo, )
+
+//onclick duck (duck disappears, points displayed on screen temporarily, player gets points, -ammo, round finishes, innerhtml for round changes +1)
+
+//duck flies away (computer +points, computer got him!, points for duck displayed by ducks exit, round finishes, innerhtml for round changes +1)
+
+//rounds on a for loop (initiated by clicking start)
+
+// out of ammo (if ammo <=0 disable onclick function for screen and duck)
+
+//calculate winner at the end of rounds(if user points is greater than computer points screen displays "winner" otherwise screen displays "practice your skills and try again")
+
+//round starts onclick button (wait a few seconds, start keyframe, )
