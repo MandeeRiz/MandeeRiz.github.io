bad1fb96d7a1cf9fb2681852d3848a8e39abd2eb
Mandee <102609655+MandeeRiz@users.noreply.github.com>
2022-05-03 14:15:29 -0400

    Add files via upload
    added shooting functionality

--- Duck Hunt/script.js
+++ Duck Hunt/script.js
@@ -1,12 +1,12 @@
 // User object (points:, Ammo,)
-
 const user={
     points: 0,
     ammo: 3
 }
 
+let ammoNum = document.querySelector(".ammoNum");
+ammoNum.textContent = user.ammo;
 //computer object(points,)
-
 const computer = {
     points: 0,
 }
@@ -17,11 +17,16 @@ const wait = (ms) => new Promise (resolve => setTimeout(resolve, ms))
 //rounds on a for loop (initiated by clicking start)
 const startGame = async () => {
     for(let roundCounter=1; roundCounter <11; roundCounter++){
+        // reset ammo
+        resetAmmo();
+        //update reset ammo
+        ammoNum.textContent = user.ammo;
         // change textcontent for round
         let roundNumber = document.querySelector(".roundNumber")
         roundNumber.textContent = roundCounter;
         console.log(roundCounter); //take this out once devlopment is complete
         await wait(2000);
+        //send duck keyframe
         duckKeyframe();
         await wait (9000);
         }
@@ -33,6 +38,7 @@ const startGame = async () => {
 
 const resetAmmo = () => {
     user.ammo = 3;
+
 }
 //-wait a few seconds
 
@@ -44,9 +50,23 @@ duck.classList.toggle("duckToggle")
 // out of ammo (if ammo <=0 disable onclick function for screen and duck re enable at start of round)
 
 // onclick screen (-player1 ammo, )
+const shotScreen = () => {
+    user.ammo -= 1;
+    ammoNum.textContent = user.ammo;
+}
+let tvScreen = document.querySelector(".tvScreen");
+tvScreen.addEventListener("click", shotScreen);
+
 
 //onclick duck (duck disappears, points displayed on screen temporarily, player gets points, -ammo, round finishes, innerhtml for round changes +1, reset ammo)
-
+const shotDuck = () => {
+    duck.classList.add("hidden");
+    console.log("I shot him!");
+    user.ammo -= 1;
+    ammoNum.textContent = user.ammo;
+}
+let duck = document.querySelector(".duck")
+duck.addEventListener("click", shotDuck)
 //duck flies away (computer +points, computer got him!, points for duck displayed by ducks exit, round finishes, innerhtml for round changes +1, reset ammo)
 //duck exits = end of round
 
